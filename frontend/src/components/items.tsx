e7e0aefd662bea8da3a2b3a014922c5d8629e12d
cyb3rflx <you@example.com>
2026-07-07 14:05:49 +0200

    WIP: List items in table

--- frontend/src/components/items.tsx
+++ frontend/src/components/items.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Item } from "@/api/items";
 import { getItems } from "@/api/items";
-import { Outlet } from "react-router";
 
 import { MoreHorizontalIcon } from "lucide-react"
 
@@ -38,84 +37,50 @@ export function ItemsTable() {
 
        loadItems()
     }, [])
-       
+
+    const dropdownMenu = 
+        <DropdownMenu>                    
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon" className="size-8">
+                <MoreHorizontalIcon />
+                <span className="sr-only">Open menu</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem>Edit</DropdownMenuItem>
+                <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem variant="destructive">
+                Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
+    const listItems = items.map(item =>
+        <TableRow>
+            <TableCell className="font-medium">{item.title}</TableCell>
+            <TableCell>{item.description}</TableCell>
+            <TableCell>{item.created_at}</TableCell>
+            <TableCell>{item.updated_at}</TableCell>
+            <TableCell className="text-right">
+                {dropdownMenu}
+            </TableCell>
+        </TableRow>
+    )
     
     return (
         <Table>
             <TableHeader>
                 <TableRow>
-                <TableHead>Product</TableHead>
-                <TableHead>Price</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Created at</TableHead>
+                <TableHead>Updated at</TableHead>
                 <TableHead className="text-right">Actions</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                <TableCell className="font-medium">Wireless Mouse</TableCell>
-                <TableCell>$29.99</TableCell>
-                <TableCell className="text-right">
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon" className="size-8">
-                        <MoreHorizontalIcon />
-                        <span className="sr-only">Open menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem variant="destructive">
-                        Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                    </DropdownMenu>
-                </TableCell>
-                </TableRow>
-                <TableRow>
-                <TableCell className="font-medium">Mechanical Keyboard</TableCell>
-                <TableCell>$129.99</TableCell>
-                <TableCell className="text-right">
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon" className="size-8">
-                        <MoreHorizontalIcon />
-                        <span className="sr-only">Open menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem variant="destructive">
-                        Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                    </DropdownMenu>
-                </TableCell>
-                </TableRow>
-                <TableRow>
-                <TableCell className="font-medium">USB-C Hub</TableCell>
-                <TableCell>$49.99</TableCell>
-                <TableCell className="text-right">
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon" className="size-8">
-                        <MoreHorizontalIcon />
-                        <span className="sr-only">Open menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem variant="destructive">
-                        Delete
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                    </DropdownMenu>
-                </TableCell>
-                </TableRow>
+                {listItems}
             </TableBody>
         </Table>
     )
